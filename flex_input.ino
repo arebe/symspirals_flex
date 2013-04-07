4930d5f7beeb446aa57a6a4f046df299cdcd13fa
RB <rfboyce@gmail.com>
2013-04-07 19:56:04 -0400

    updated comments in flex_input.ino

--- flex_input.ino
+++ flex_input.ino
@@ -1,4 +1,8 @@
-
+// flex_input.ino
+// Based on the SIK Circuit #9 flex sensor
+// uses serial handshaking to establish contact w Processing
+// rfboyce@gmail.com
+// 07 Apr 2013
 
 // Define the analog input pin to measure flex sensor position:
 
@@ -7,8 +11,7 @@ const int flexpin = 0;
 int inByte = 0;         // incoming serial byte
 
 
-void setup() 
-{ 
+void setup() { 
   // Use the serial monitor window to help debug our sketch:
    
   Serial.begin(9600);
@@ -17,8 +20,7 @@ void setup()
 } 
 
 
-void loop() 
-{ 
+void loop() { 
   int flexposition;    // Input value from the analog pin.
 
  // if we get a valid byte, read analog ins:
@@ -29,13 +31,13 @@ void loop()
   // Read the position of the flex sensor (0 to 1023):
   
   flexposition = analogRead(flexpin);
+
+  // change the bwlo values to correspond to the range of flex sensor readings
+  // and re-map to 0-255
   flexposition = map(flexposition, 690, 910, 0, 255);
   flexposition = constrain(flexposition, 0, 255);
 
   Serial.println(flexposition);
-
-  
-
   delay(20); 
   }
 } 
